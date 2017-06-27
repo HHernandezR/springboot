26fd204a8e0f24f6f9ff4758d26299f2b8dd67d8
Horacio Hernandez <hhernandez@hopewellsystem.com>
2017-06-27 09:53:50 -0500

    * Agregado Proyecto Nuevo Spring Boot Heroes * Back End realizado con Spring Boot * Front End realizado con Angular 2 * Base de datos Mongo

---
+++ SpringBootHeroes/src/main/java/edu/spring4/SpringBootHeroes/SpringBootHeroesApplication.java
@@ -0,0 +1,12 @@
+package edu.spring4.SpringBootHeroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHeroesApplication.class, args);
+	}
+}
